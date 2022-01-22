95eb0f0930b4d335741b3b52f4c8ea85a65c146e
codepip2007 <pepcplace@gmail.com>
2022-01-22 11:07:09 +0800

    added academy pages

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
